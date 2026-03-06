c9e668b2619445b0cf4589f0c2dd9b2cbae63c45
vividron <ronaks21205@gmail.com>
2026-03-06 18:32:00 +0530

    refactor: add custom error handling for socket authentication

--- sockets/middleware/auth.socket.js
+++ sockets/middleware/auth.socket.js
@@ -1,23 +1,17 @@
 import jwt from "jsonwebtoken";
+import AppError from "../../utils/appError.js";
 
 export const socketAuth = (socket, next) => {
     try {
-        const token = socket.handshake.auth?.token ||
-            socket.handshake.query?.token;;
-        if (!token) {
-            const err = new Error("Token not found");
-            err.code = "INVALID_TOKEN";
-            return next(err);
-        };
+        const token = socket.handshake.auth?.token || socket.handshake.query?.token;
+        if (!token) new AppError("Token not found", "INVALID_TOKEN", null)
 
         const decode = jwt.verify(token, process.env.JWT_SECRET);
         socket.userId = decode.userId;
         return next();
 
     } catch (err) {
-        const error = new Error("Not authorized");
-        error.code = "INVALID_TOKEN";
-        if (err.name === "TokenExpiredError") error.message = "Token expired";
-        next(error);
+        if (err.name === "TokenExpiredError") next(new AppError("Token expired", "TOKEN_EXPIRED", null));
+        next(new AppError("Not authorized", "INVALID_TOKEN", null, err));
     }
 }
