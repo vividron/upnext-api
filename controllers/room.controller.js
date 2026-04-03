63af525a4b7c260155e7254d584f2cfbb1c09cbc
vividron <ronaks21205@gmail.com>
2026-04-04 00:44:12 +0530

    fix(room): fix room route handler bugs.

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -1,9 +1,21 @@
 import Room from "../models/Room.js";
-import * as roomService  from "../services/room.service.js";
+import User from "../models/User.js"
+import * as roomService from "../services/room.service.js";
+import AppError from "../utils/appError.js";
 import asyncWrapper from "../utils/asyncWrapper.js";
 
 export const createRoom = asyncWrapper(async (req, res) => {
     const { title } = req.body;
+    const userId = req.userId;
+
+    // Check if user has spotify premium. (we can control player only if user has spotify premium)
+    const user = await User.findById(userId).select("isPremium");
+
+    if (!user) throw new AppError("User not found", "USER_NOT_FOUND", 400);
+
+    const { isPremium } = user;
+    if (!isPremium) throw new AppError("Spotify Premium is required to create a room", "USER_NOT_PREMIUM", 403);
+
     const room = await Room.create({
         host: req.userId,
         title: title || `Room-${Date.now()}`,
@@ -12,7 +24,7 @@ export const createRoom = asyncWrapper(async (req, res) => {
     res.status(200).json({
         success: true,
         room: {
-            roomId: room._id,
+            _id: room._id,
             title: room.title,
             createdAt: room.createdAt
         }
@@ -20,7 +32,7 @@ export const createRoom = asyncWrapper(async (req, res) => {
 });
 
 export const getRooms = asyncWrapper(async (req, res) => {
-    const rooms = await Room.find({ host: req.userId }, "title createdAt");
+    const rooms = await Room.find({ host: req.userId }, "title createdAt isActive").sort({ createdAt: -1 });
 
     res.status(200).json({
         success: true,
@@ -54,12 +66,18 @@ export const leaveRoom = asyncWrapper(async (req, res) => {
 });
 
 export const getRoom = asyncWrapper(async (req, res) => {
-    const {roomId} = req.params;
+    const { roomId } = req.params;
     const userId = req.userId;
+
+    // Check if user joined the room and get the room state
+    const isUserJoined = await roomService.isUserJoined(roomId, userId);
+    if (!isUserJoined) {
+        throw new AppError("User did not join the room", "USER_NOT_FOUND", 400);
+    }
 
     const roomState = await roomService.getRoomState(roomId, userId);
 
-    if(!roomState) {
+    if (!roomState) {
         throw new AppError("Room not found or the room is not active", "ROOM_NOT_FOUND", 404);
     }
 
@@ -76,7 +94,7 @@ export const deleteRoom = asyncWrapper(async (req, res) => {
     const room = await Room.findOne({ _id: roomId, host: userId });
 
     // Check if the room exist
-     if (!room) {
+    if (!room) {
         throw new AppError("Room not found", "ROOM_NOT_FOUND", 404);
     }
 
